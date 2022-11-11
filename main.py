f6f3d29156e9ffa28538b8df9fece603a32f52d4
wayner0628 <a901639@gmail.com>
2022-11-11 19:31:05 +0000

    Implement model and Output testing data

--- main.py
+++ main.py
@@ -1,16 +1,90 @@
 import pandas as pd
-from keras.preprocessing import sequence
-from keras.preprocessing.text import Tokenizer
-
-df = pd.read_csv("./train_HW2dataset.csv")
-
-entry_size = df.shape[0]
-sentences = []
-labels = []
+import tensorflow as tf
+from keras import layers, losses
+from keras.layers import TextVectorization
 
 
-for entry in range(entry_size):
-    sentences.append(df["Utterance"][entry])
+label_map = {"neutral": 0, "anger": 1, "joy": 2, "surprise": 3, "sadness": 4, "disgust": 5, "fear": 6}
 
-# sentences = tf.convert_to_tensor([1, 1])
-# print(sentences)
+
+class Preprocessing:
+    def __init__(self):
+        self.data = "Dataset/train_HW2dataset.csv"
+        self.max_len = 30
+        self.max_words = 50000
+
+    def load_data(self):
+        df = pd.read_csv(self.data)
+        df = df.drop(
+            [
+                "Speaker",
+                "Dialogue_ID",
+                "Utterance_ID",
+                "Old_Dialogue_ID",
+                "Old_Utterance_ID",
+                "Season",
+                "Episode",
+                "StartTime",
+                "EndTime",
+            ],
+            axis=1,
+            inplace=False,
+        )
+
+        X = df["Utterance"].values
+        Y = df["Emotion"].values
+
+        self.x_train, self.y_train = X, Y
+        return df
+
+    def prepare_tokens(self):
+        vectorize_layer = TextVectorization(
+            max_tokens=self.max_words, output_mode="int", output_sequence_length=self.max_len
+        )
+        vectorize_layer.adapt(self.x_train)
+
+        return vectorize_layer(self.x_train)
+
+
+if __name__ == "__main__":
+    preprocess = Preprocessing()
+    preprocess.load_data()
+    token = preprocess.prepare_tokens()
+    label = tf.convert_to_tensor([label_map[label] for label in preprocess.y_train])
+
+    ds = tf.data.Dataset.from_tensor_slices((token, label))
+    ds = ds.batch(32)
+
+    # AUTOTUNE = tf.data.AUTOTUNE
+    # ds = ds.cache().prefetch(buffer_size=AUTOTUNE)
+
+    embedding_dim = 16
+    model = tf.keras.Sequential(
+        [
+            layers.Embedding(50000 + 1, embedding_dim),
+            layers.Dropout(0.2),
+            # layers.Conv1D(120, 3, activation='relu'),
+            layers.GlobalAveragePooling1D(),
+            layers.Dropout(0.2),
+            layers.Dense(30, activation="relu"),
+            layers.Dropout(0.2),
+            layers.Dense(7, activation="relu"),
+            layers.Softmax(),
+        ]
+    )
+
+    model.compile(loss=losses.SparseCategoricalCrossentropy(from_logits=False), optimizer="adam", metrics=["accuracy"])
+
+    epochs = 10
+    history = model.fit(ds, epochs=epochs)
+    history_dict = history.history
+    history_dict.keys()
+
+    acc = history_dict["accuracy"]
+    loss = history_dict["loss"]
+
+    epochs = range(1, len(acc) + 1)
+
+    # examples = ["My duties?  All right.", "No don’t I beg of you!", "I see."]
+
+    model.save("saved_model/my_model")
---
+++ predict.py
@@ -0,0 +1,27 @@
+from main import Preprocessing
+import pandas as pd
+import numpy as np
+import tensorflow as tf
+
+
+df = pd.read_csv("./Dataset/test_HW2dataset.csv")
+X = df["Utterance"].values
+
+preprocess = Preprocessing()
+preprocess.x_train = X
+
+token = preprocess.prepare_tokens()
+
+new_model = tf.keras.models.load_model('saved_model/my_model')
+new_model.summary()
+
+res = new_model.predict(token)
+res = tf.math.argmax(res, axis=1)
+res = res.numpy()
+
+index = np.arange(3400)
+
+data = {'index': index, 'emotion': res}
+
+DF = pd.DataFrame.from_dict(data)
+DF.to_csv('output.csv')
